c95e5020e4e189f5f9a89857d6a2414c6a03a4e9
Daren Shamoun <darenshamoun@gmail.com>
2023-11-11 14:23:17 -0800

    introduced the property tile component

--- frontend/src/components/PropertyTile.js
+++ frontend/src/components/PropertyTile.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const PropertyTile = ({ property, onClick }) => {
+const PropertyTile = ({ property }) => {
     return (
-        <div className="border p-4 rounded shadow" onClick={() => onClick(property.id)}>
-            <h2 className="font-bold">{property.name}</h2>
-            <p>Type: {property.property_type}</p>
-            {/* Add more property details you wish to display */}
+        <div className="border p-4 rounded-lg shadow-lg m-4 hover:shadow-xl transition duration-300 ease-in-out">
+            <h3 className="text-lg font-semibold">{property.name}</h3>
+            <p>Type: {property.type}</p>
+            <p>Location: {property.location}</p>
+            {/* Add more property details you want to display */}
+            <button className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                View Details
+            </button>
         </div>
     );
 };
