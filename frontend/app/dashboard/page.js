4900696139b59ea5200656041c51438ece693a0c
Daren Shamoun <darenshamoun@gmail.com>
2023-11-12 21:25:09 -0800

    reverted to basics for home page

--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import Layout from '../layout'; // Adjust the import path to your layout component
-import PropertySummary from '../components/PropertySummary'; // A new component to be created
 
 export default function DashboardPage() {
     return (
         <Layout>
             <main className="container mx-auto p-4">
                 <h1 className="text-2xl font-bold mb-6">Dashboard</h1>
-                {/* You can add more dashboard-specific components here */}
-                <PropertySummary />
-                {/* Add other summaries like TenantSummary, LeaseSummary, etc. */}
             </main>
         </Layout>
     );
