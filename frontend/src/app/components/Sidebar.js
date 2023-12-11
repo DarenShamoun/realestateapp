f4bb8bce0aa229bcee386a49d1512ae1a5d4a362
DarenShamoun <darenshamoun@gmail.com>
2023-12-11 13:10:25 -0800

    updated sidebar styling

--- frontend/src/app/components/Sidebar.js
+++ frontend/src/app/components/Sidebar.js
@@ -1,22 +1,24 @@
+// Sidebar.js
 import Link from 'next/link';
 
 const Sidebar = () => {
   return (
-    <div className="bg-gray-700 text-white w-64 min-h-screen p-5">
-      <ul>
-        <li className="mb-6">
-          <Link href="/">Home</Link>
-        </li>
-        <li className="mb-6">
-          <Link href="/properties">Properties</Link>
-        </li>
-        <li className="mb-6">
-          <Link href="/tenants">Tenants</Link>
-        </li>
-        <li className="mb-6">
-          <Link href="/financials">Financials</Link>
-        </li>
-      </ul>
+    <div className="w-64 min-w-[250px] bg-gradient-to-b from-gray-700 to-gray-900 p-5 shadow-xl">
+      <nav className="mt-10">
+        {[
+          { name: 'Home', href: '/' },
+          { name: 'Properties', href: '/properties' },
+          { name: 'Tenants', href: '/tenants' },
+          { name: 'Financials', href: '/financials' },
+        ].map((link) => (
+          // Update Link usage here as per new Next.js documentation
+          <Link key={link.name} href={link.href}>
+            <span className="block cursor-pointer mb-6 p-3 rounded-lg transition duration-300 ease-in-out transform hover:translate-x-2 hover:bg-gray-600 hover:shadow">
+              {link.name}
+            </span>
+          </Link>
+        ))}
+      </nav>
     </div>
   );
 };
