00a36ee37294661c1d44951f0497fc7177b1fda7
Daren Shamoun <darenshamoun@gmail.com>
2023-12-16 23:05:33 -0800

    added an interface for adding new properties to the database

---
+++ frontend/src/components/Modals/AddPropertyModal.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import { addProperty } from '@/api/propertyService';
+
+const AddPropertyModal = ({ show, onClose }) => {
+    const [formData, setFormData] = useState({
+      name: '',
+      property_type: '',
+      address: '',
+      purchase_price: '',
+      year_built: '',
+      square_footage: ''
+    });
+  
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        await addProperty(formData);
+        onClose(); // Close the modal on success
+      } catch (error) {
+        console.error('Error adding property:', error);
+      }
+    };
+  
+    if (!show) {
+      return null;
+    }
+  
+    return (
+      <div className="fixed inset-0 bg-black bg-opacity-50 overflow-y-auto h-full w-full flex justify-center items-center">
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <div className="p-6 text-center">
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Add New Property</h3>
+            <form onSubmit={handleSubmit} className="space-y-6">
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Property Name"
+                className="w-full p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                required
+              />
+              <input
+                type="text"
+                name="property_type"
+                value={formData.property_type}
+                onChange={handleChange}
+                placeholder="Property Type"
+                className="w-full p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                required
+              />
+              <input
+                type="text"
+                name="address"
+                value={formData.address}
+                onChange={handleChange}
+                placeholder="Address"
+                className="w-full p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                required
+              />
+              <input
+                type="number"
+                name="purchase_price"
+                value={formData.purchase_price}
+                onChange={handleChange}
+                placeholder="Purchase Price"
+                className="w-full p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              />
+              <input
+                type="number"
+                name="year_built"
+                value={formData.year_built}
+                onChange={handleChange}
+                placeholder="Year Built"
+                className="w-full p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              />
+              <input
+                type="number"
+                name="square_footage"
+                value={formData.square_footage}
+                onChange={handleChange}
+                placeholder="Square Footage"
+                className="w-full p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              />
+              <div className="flex justify-end gap-4">
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Property</button>
+                <button onClick={onClose} type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 rounded-lg border border-gray-300 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-700">Cancel</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+};
+  
+export default AddPropertyModal;
+  
--- frontend/src/components/Properties.js
+++ frontend/src/components/Properties.js
@@ -3,9 +3,11 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { getProperties } from '../api/propertyService';
+import AddPropertyModal from './Modals/AddPropertyModal';
 
 const Properties = () => {
   const [properties, setProperties] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     getProperties().then(data => {
@@ -15,22 +17,37 @@ const Properties = () => {
     });
   }, []);
 
+  const handleAddClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <section>
-      <div className="flex m-4 gap-2">
+      <div className="flex justify-between items-center p-4">
+        <h2 className="text-2xl text-white font-bold">Properties</h2>
+        <button onClick={handleAddClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Add New Property
+        </button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
         {properties.map(property => (
-          <Link key={property.id} href={`/properties/${property.id}`} className="property-card bg-gray-700 shadow rounded p-4 m-2 cursor-pointer">
+          <Link key={property.id} href={`/properties/${property.id}`} className="property-card bg-gray-700 shadow rounded p-4 cursor-pointer hover:bg-gray-600 transition ease-in-out duration-150">
               <h3 className="text-white font-bold">{property.name}</h3>
               <p className="text-gray-300">{property.address}</p>
               <p className="text-gray-300">{property.property_type}</p>
-              <p className="text-gray-300"> Built in {property.year_built}</p>
+              <p className="text-gray-300">Built in {property.year_built}</p>
               <p className="text-gray-300">{property.square_footage} Square Feet</p>
           </Link>
         ))}
       </div>
+      <AddPropertyModal show={showModal} onClose={handleCloseModal} />
     </section>
+
   );
 };
-
 
 export default Properties;
