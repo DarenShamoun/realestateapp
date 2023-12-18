3819f5faea261cdaaec156d6a9e1accdca1e86cc
Daren Shamoun <darenshamoun@gmail.com>
2023-12-18 13:42:05 -0800

    added the ability to edit delete and add properties in the management menu

--- frontend/src/components/Cards/PropertyCard.js
+++ frontend/src/components/Cards/PropertyCard.js
@@ -3,33 +3,23 @@
 import React from 'react';
 import Link from 'next/link';
 
-const PropertyCard = ({ property, onEdit, onDelete, isManagementMode }) => {
-  const cardContent = (
-    <>
+const PropertyCard = ({ property, isManagementMode, onEdit, onDelete }) => {
+  return (
+    <div className="property-card bg-gray-700 shadow rounded p-4 hover:bg-gray-600 transition ease-in-out duration-150">
       <h3 className="text-white font-bold">{property.name}</h3>
       <p className="text-gray-300">{property.address}</p>
       <p className="text-gray-300">{property.property_type}</p>
       <p className="text-gray-300">Built in {property.year_built}</p>
       <p className="text-gray-300">{property.square_footage} Square Feet</p>
-      {isManagementMode && (
-        <button onClick={(e) => { e.stopPropagation(); onDelete(); }} className="text-red-500 hover:text-red-700 mt-2">
-          Delete
-        </button>
+      {isManagementMode ? (
+        <div className="flex justify-end space-x-2 mt-4">
+          <button onClick={() => onEdit(property)} className="text-blue-500 hover:text-blue-700">Edit</button>
+          <button onClick={() => onDelete(property.id)} className="text-red-500 hover:text-red-700">Delete</button>
+        </div>
+      ) : (
+        <Link href={`/properties/${property.id}`} className="text-blue-500 hover:text-blue-700">View Details</Link>
       )}
-    </>
-  );
-
-  return isManagementMode ? (
-    <div
-      className="property-card bg-gray-700 shadow rounded p-4 cursor-pointer hover:bg-gray-600 transition ease-in-out duration-150"
-      onClick={() => onEdit(property)}
-    >
-      {cardContent}
     </div>
-  ) : (
-    <Link href={`/properties/${property.id}`} className="property-card bg-gray-700 shadow rounded p-4 cursor-pointer hover:bg-gray-600 transition ease-in-out duration-150">
-      {cardContent}
-    </Link>
   );
 };
 
