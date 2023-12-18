58d7391ec3c0a638c793d3a562ed2813168f365d
Daren Shamoun <darenshamoun@gmail.com>
2023-12-17 22:00:58 -0800

    changed the button interaction with the units

--- frontend/src/components/Cards/UnitCard.js
+++ frontend/src/components/Cards/UnitCard.js
@@ -9,16 +9,17 @@ const UnitCard = ({ unit }) => {
   };
 
   return (
-    <div className="unit-card bg-gray-700 shadow rounded p-4 hover:bg-gray-600 transition ease-in-out duration-150">
-      <h3 className="text-white font-bold">Unit {unit.unit_number}</h3>
-      <p className="text-gray-300">Occupied: {unit.is_occupied ? 'Yes' : 'No'}</p>
-      {unit.is_occupied && unit.tenant && (
-        <p className="text-gray-300">Tenant: {unit.tenant.full_name}</p>
-      )}
-      <p className="text-gray-300">Total Rent: ${displayTotalRent(unit.total_rent)}</p>
-      {/* Additional unit details */}
-      <Link href={`/units/${unit.id}`} className="text-blue-500 hover:text-blue-700">View Details</Link>
-    </div>
+    <Link href={`/units/${unit.id}`} className="unit-card bg-gray-700 shadow rounded p-4 cursor-pointer hover:bg-gray-600 transition ease-in-out duration-150">
+      <div>
+        <h3 className="text-white font-bold">Unit {unit.unit_number}</h3>
+        <p className="text-gray-300">Occupied: {unit.is_occupied ? 'Yes' : 'No'}</p>
+        {unit.is_occupied && unit.tenant && (
+          <p className="text-gray-300">Tenant: {unit.tenant.full_name}</p>
+        )}
+        <p className="text-gray-300">Total Rent: ${displayTotalRent(unit.total_rent)}</p>
+        {/* Additional unit details */}
+      </div>
+    </Link>
   );
 };
 
