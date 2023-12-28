550a2712529a3cd4c952a8a0989f6dda469fd721
Daren Shamoun <darenshamoun@gmail.com>
2023-12-27 19:32:41 -0800

    Revert migration and update unit_service.py

--- backend/models.py
+++ backend/models.py
@@ -27,11 +27,9 @@ class Unit(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     property_id = db.Column(db.Integer, db.ForeignKey('property.id'), nullable=False, index=True)
-    tenant_id = db.Column(db.Integer, db.ForeignKey('tenant.id'), nullable=True, index=True)
-    tenant = db.relationship('Tenant', backref='units', uselist=False)
     unit_number = db.Column(db.String(20), nullable=False)
-    rent_details = db.relationship('Rent', backref='unit', uselist=False, lazy=True)
-    rent_status = db.Column(db.String(50))  # Possible values: 'Current', 'Late', 'Paid'
+    lease_details = db.relationship('Lease', backref='unit', lazy=True)
+    rent_status = db.Column(db.String(50))
     is_occupied = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
@@ -41,7 +39,7 @@ class Rent(db.Model):
     Represents the rent details for a unit in the real estate application.
     """
     id = db.Column(db.Integer, primary_key=True)
-    unit_id = db.Column(db.Integer, db.ForeignKey('unit.id'), nullable=False, index=True)
+    lease_id = db.Column(db.Integer, db.ForeignKey('lease.id'), nullable=False, index=True)
     rent = db.Column(db.Float, nullable=False, default=0)
     trash = db.Column(db.Float, nullable=False, default=0)
     water_sewer = db.Column(db.Float, nullable=False, default=0)
@@ -55,7 +53,7 @@ class Rent(db.Model):
 
 class RentHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    unit_id = db.Column(db.Integer, db.ForeignKey('unit.id'), nullable=False)
+    lease_id = db.Column(db.Integer, db.ForeignKey('lease.id'), nullable=False)
     old_rent = db.Column(db.Float, nullable=False)
     new_rent = db.Column(db.Float, nullable=False)
     change_date = db.Column(db.DateTime, default=datetime.utcnow)
@@ -87,7 +85,9 @@ class Lease(db.Model):
     deposit = db.Column(db.Float)
     terms = db.Column(db.Text)
     status = db.Column(db.String(50))  # Possible values: 'Active', 'Ended', 'Renewed'
-    payments = db.relationship('Payment', backref='lease', lazy=True)
+    rents = db.relationship('Rent', backref='lease', lazy=True)
+    payment_id = db.Column(db.Integer, db.ForeignKey('payment.id'), nullable=True, index=True)
+    payments = db.relationship('Payment', backref='lease', lazy=True, foreign_keys=[payment_id])
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
@@ -97,15 +97,11 @@ class Payment(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     lease_id = db.Column(db.Integer, db.ForeignKey('lease.id'), nullable=False, index=True)
-    tenant_id = db.Column(db.Integer, db.ForeignKey('tenant.id'), index=True)
-    unit_id = db.Column(db.Integer, db.ForeignKey('unit.id'), index=True)
     date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     amount = db.Column(db.Float, nullable=False)
     payment_method = db.Column(db.String(50), nullable=True)
     receipt_number = db.Column(db.String(100), unique=True)
     is_partial = db.Column(db.Boolean, default=False)
-    tenant = db.relationship('Tenant', backref='payments')
-    unit = db.relationship('Unit', backref='payments')
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
--- backend/services/unit_service.py
+++ backend/services/unit_service.py
@@ -34,8 +34,8 @@ def delete_unit(unit_id):
 
 def get_total_rent(unit_id):
     unit = Unit.query.get(unit_id)
-    if unit and unit.rent_details:
-        return unit.rent_details.total_rent
+    if unit:
+        return sum(lease.rent.total_rent for lease in unit.lease_details if lease.rent)
     return 0
 
 def calculate_balance(unit_id, year, month):
@@ -52,34 +52,21 @@ def calculate_balance(unit_id, year, month):
 def update_balance(unit_id, year, month):
     unit = Unit.query.get(unit_id)
     if unit:
-        total_paid = sum(payment.amount for payment in unit.payments if payment.date.year == year and payment.date.month == month)
-        balance = get_total_rent(unit_id) - total_paid
-        if balance > 0:
-            unit.rent_details.debt += balance
-        elif balance < 0:
-            unit.rent_details.debt = max(0, unit.rent_details.debt + balance)
-        db.session.commit()
+        total_paid = sum(payment.amount for lease in unit.lease_details for payment in lease.payments if payment.date.year == year and payment.date.month == month)
+        total_rent = sum(lease.rent.total_rent for lease in unit.lease_details if lease.rent and lease.rent.date.year == year and lease.rent.date.month == month)
+        balance = total_rent - total_paid
         return balance
     return None
 
+
 def unit_to_json(unit):
-    rent_details = unit.rent_details
     return {
         'id': unit.id,
         'property_id': unit.property_id,
         'unit_number': unit.unit_number,
         'is_occupied': unit.is_occupied,
         'rent_status': unit.rent_status,
-        'tenant_id': unit.tenant_id,
-        'total_rent': get_total_rent(unit.id) if rent_details else None,
-        'created_at': unit.created_at,
-        'updated_at': unit.updated_at,
-        'rent_details': {
-            'rent': rent_details.rent if rent_details else 0,
-            'trash': rent_details.trash if rent_details else 0,
-            'water_sewer': rent_details.water_sewer if rent_details else 0,
-            'parking': rent_details.parking if rent_details else 0,
-            'debt': rent_details.debt if rent_details else 0,
-            'breaks': rent_details.breaks if rent_details else 0,
-        } if rent_details else {}
+        'total_rent': get_total_rent(unit.id),
+        'created_at': unit.created_at.isoformat(),
+        'updated_at': unit.updated_at.isoformat()
     }
