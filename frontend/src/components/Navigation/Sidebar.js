9345e741f5b6d599818b34f36ee6589deb126d6f
Daren Shamoun <darenshamoun@gmail.com>
2024-01-02 18:13:23 -0800

    Fix aria-label typo in Sidebar component

--- frontend/src/components/Navigation/Sidebar.js
+++ frontend/src/components/Navigation/Sidebar.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Sidebar = () => {
   return (
     <>
-      <aside className="fixed top-0 left-0 w-64 h-full" aria label="Sidenav">
+      <aside className="fixed top-0 left-0 w-64 h-full" aria-label="Sidenav">
         <div className="overflow-y-auto py-5 px-3 h-full bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
           <ul className="space-y-2">
             <li>
@@ -36,7 +36,9 @@ const Sidebar = () => {
               <Link href="/documents">
                 <button type="button" className="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-sales" data-collapse-toggle="dropdown-sales">
                   <span className="flex-1 ml-3 text-left whitespace-nowrap">Documents</span>
-                  <svg aria-hidden="true" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                  <svg aria-hidden="true" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                  </svg>
                 </button>
               </Link>
             </li>
