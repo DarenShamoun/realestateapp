4039822bac8a28a19390b716eab9c9d063d15762
Daren Shamoun <darenshamoun@gmail.com>
2024-01-13 17:21:40 -0800

    Fix import statements in UnitDetails.js, CreateLeaseModal.js, CreatePaymentModal.js, and CreateRentModal.js

--- frontend/src/components/Details/UnitDetails.js
+++ frontend/src/components/Details/UnitDetails.js
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 import { useUnitDetails } from '@/hooks/useUnitDetails';
 import BarChartPlot from "@/components/Charts/BarChartPlot";
--- frontend/src/components/Modals/CreateLeaseModal.js
+++ frontend/src/components/Modals/CreateLeaseModal.js
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import { getTenants } from '@/api/tenantService';
 import { addTenant } from '@/api/tenantService';
