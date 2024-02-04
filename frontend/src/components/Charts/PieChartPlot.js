2c108fe3a61f255d7db3f2b47643024191765b1e
Daren Shamoun <darenshamoun@gmail.com>
2024-02-03 19:10:37 -0800

    Update colors in PieChartPlot component

--- frontend/src/components/Charts/PieChartPlot.js
+++ frontend/src/components/Charts/PieChartPlot.js
@@ -37,10 +37,8 @@ const renderCustomizedLabel = ({
 };
 
 const PieChartPlot = ({ data, title }) => {
-  const colors = [
-    "rgba(130, 202, 157, 0.8)", // Light green with opacity
-    "rgba(250, 128, 114, 0.8)"  // Light red with opacity
-  ];
+  const colors = ["#82ca9d", "#FA8072"];
+
   const defaultData = [
     { name: "Rent Paid", value: 20000 },
     { name: "Remaining Rent", value: 5000 }
