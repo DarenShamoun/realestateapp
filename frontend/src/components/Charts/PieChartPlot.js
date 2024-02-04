8efb77b44cbb70c2b92f3819b6fb1a0e71b562b5
Daren Shamoun <darenshamoun@gmail.com>
2024-02-03 19:38:42 -0800

    Add useState hook and renderActiveShape function to PieChartPlot.js

--- frontend/src/components/Charts/PieChartPlot.js
+++ frontend/src/components/Charts/PieChartPlot.js
@@ -1,38 +1,50 @@
-import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer, Legend } from "recharts";
+import { useState } from 'react';
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer, Legend, Sector } from "recharts";
 
-const CustomTooltip = ({ active, payload, label }) => {
-  if (active && payload && payload.length) {
-    return (
-      <div className="custom-tooltip" style={{ backgroundColor: '#6B7280', padding: '10px', border: '1px solid #ccc' }}>
-        <p className="label">{label}</p>
-        {payload.map((entry, index) => (
-          <p key={`item-${index}`} style={{ color: entry.color }}>
-            {`${entry.name}: $${entry.value?.toFixed(2)}`}
-          </p>
-        ))}
-      </div>
-    );
-  }
-  return null;
-};
-
-const renderCustomizedLabel = ({
-  cx, cy, midAngle, innerRadius, outerRadius, percent, payload
-}) => {
+const renderActiveShape = (props) => {
   const RADIAN = Math.PI / 180;
-  const radius = 25 + innerRadius + (outerRadius - innerRadius);
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  const {
+    cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
+    fill, payload, percent, value
+  } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? 'start' : 'end';
 
   return (
-    <>
-      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-        {`$${payload.value.toFixed(0)}`}
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill="white">{payload.name}</text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="white">{`$${value.toFixed(2)}`}</text>
+      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="white">
+        {`(${(percent * 100).toFixed(2)}%)`}
       </text>
-      <text x={x} y={y - 20} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-        {`${(percent * 100).toFixed(0)}%`}
-      </text>
-    </>
+    </g>
   );
 };
 
@@ -44,6 +56,12 @@ const PieChartPlot = ({ data, title }) => {
     { name: "Remaining Rent", value: 5000 }
   ];
 
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const onPieEnter = (_, index) => {
+    setActiveIndex(index);
+  };
+
   // Calculate the sum of values in the data
   const dataSum = data?.reduce((acc, item) => acc + item.value, 0);
 
@@ -53,29 +71,26 @@ const PieChartPlot = ({ data, title }) => {
   return (
     <>
       <h1 style={{ paddingLeft: '20px', paddingTop: '10px', color: 'white', fontWeight: 'bold' }}>{title}</h1> 
-      <ResponsiveContainer width="100%" height="100%">
-        <PieChart width={400} height={300} margin={{
-            top: 10,
-            right: 30,
-            left: 30,
-            bottom: 40,
-          }}>
+      <ResponsiveContainer width="100%" height="100%" >
+        <PieChart width={400} height={400} margin={{ top: 20, right: 20, bottom: 40, left: 20 }}>
           <Pie
+            activeIndex={activeIndex}
+            activeShape={renderActiveShape}
             data={chartData}
             dataKey="value"
             nameKey="name"
             cx="50%"
             cy="50%"
+            innerRadius={60}
             outerRadius={85}
             fill="#8884d8"
-            label={renderCustomizedLabel}
+            onMouseEnter={onPieEnter}
             >
             {chartData.map((entry, index) => (
               <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
             ))}
           </Pie>
-          <Legend />
-          <Tooltip content={<CustomTooltip />} />
+          <Legend verticalAlign="bottom" />
         </PieChart>
       </ResponsiveContainer>
     </>
