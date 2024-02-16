8ac18cfc08eeec3ca9581cc1788aa282115f681a
Daren Shamoun <darenshamoun@gmail.com>
2024-02-15 17:21:31 -0800

    Refactor document buttons layout

--- frontend/src/components/Pages/Documents.js
+++ frontend/src/components/Pages/Documents.js
@@ -473,15 +473,15 @@ const Documents = () => {
                             <span className="text-lg text-white">{doc.custom_filename || doc.filename}</span>
                             <div className="flex items-center">
                                 {/* Download icon */}
-                                <button onClick={() => handleDownloadDocument(doc.id)} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-2 rounded mx-2">
+                                <button onClick={() => handleDownloadDocument(doc.id)} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-2 rounded mx-1">
                                     <img src="/download.svg" alt="Download" className="h-5 w-5" />
                                 </button>
                                 {/* View icon */}
-                                <button onClick={() => handleViewDocument(doc.id)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded mx-2">
+                                <button onClick={() => handleViewDocument(doc.id)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded mx-1">
                                     <img src="/view.svg" alt="View" className="h-5 w-5" />
                                 </button>
                                 {/* Delete icon */}
-                                <button onClick={() => handleDelete(doc.id)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-2 rounded">
+                                <button onClick={() => handleDelete(doc.id)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-2 rounded mx-1">
                                     <img src="/trash-can.svg" alt="Delete" className="h-5 w-5" />
                                 </button>
                             </div>
