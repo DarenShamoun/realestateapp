232f19a32bc77595d97ae511bb6f41d7a6da38ae
Daren Shamoun <darenshamoun@gmail.com>
2024-02-15 21:53:21 -0800

    Update button color in FinancialHistoryCard.js

--- frontend/src/components/Cards/FinancialHistoryCard.js
+++ frontend/src/components/Cards/FinancialHistoryCard.js
@@ -107,7 +107,7 @@ const FinancialHistory = ({ payments, rentHistory, leases, onOpenCreatePayment, 
             {hasActiveLease && (
               <button
                 onClick={onOpenCreateRent}
-                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4"
+                className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mt-4"
               >
                 Add Rent
               </button>
