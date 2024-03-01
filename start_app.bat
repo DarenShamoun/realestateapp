479e8658010f5641e2e181570784a7da3ee7477a
Daren Shamoun <darenshamoun@gmail.com>
2024-03-01 14:52:57 -0800

    Update start_app.bat script to open a new command prompt window for backend server

--- start_app.bat
+++ start_app.bat
@@ -7,10 +7,10 @@ SET "scriptPath=%~dp0"
 :: Navigate to the frontend directory and run commands
 cd "%scriptPath%frontend"
 call npm run build
-call npm start
+start call npm start
 
 :: Open a new command prompt window to start the backend server
-start /b cmd /c "cd %scriptPath%backend && .\venv\Scripts\activate && flask run"
+start cmd /k "cd %scriptPath%backend && .\venv\Scripts\activate && flask run"
 
 :: Return to the original script directory (optional)
 cd "%scriptPath%"
